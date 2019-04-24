c7737afd20f757f3c12c63f1a908f37319bc2b08
Кузьмин Анатолий Сергеевич <akuzmin@pmpractice.ru>
2019-04-24 19:06:17 +0300

    promisefication

---
+++ JS/promisefication.js
@@ -0,0 +1,40 @@
+
+/*Промисификация – это когда берут асинхронный функционал и делают для него обёртку, возвращающую промис.
+После промисификации использование функционала зачастую становится гораздо удобнее.
+В качестве примера сделаем такую обёртку для запросов при помощи XMLHttpRequest.
+Функция httpGet(url) будет возвращать промис, который при успешной загрузке данных с url будет переходить в fulfilled с этими данными,
+а при ошибке – в rejected с информацией об ошибке: */
+debugger;   
+function httpGet(url) {
+
+    return new Promise(function(resolve, reject) {
+  
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url, true);
+  
+      xhr.onload = function() {
+        if (this.status == 200) {
+          resolve(this.response);
+        } else {
+          var error = new Error(this.statusText);
+          error.code = this.status;
+          reject(error);
+        }
+      };
+  
+      xhr.onerror = function() {
+        reject(new Error("Network Error"));
+      };
+  
+      xhr.send();
+    });
+  
+}
+  /*Как видно, внутри функции объект XMLHttpRequest создаётся и отсылается как обычно,
+    при onload/onerror вызываются, соответственно, resolve (при статусе 200) или reject. */
+
+httpGet("C:/Users/akuzmin/Desktop/studyjs/server/user.json")
+.then(
+    response => alert(`Fulfilled: ${response}`),
+    error => alert(`Rejected: ${error}`)
+);
