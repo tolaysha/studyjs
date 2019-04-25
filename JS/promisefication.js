68283c427435692eefef7676e191de2a9b06bc16
Кузьмин Анатолий Сергеевич <akuzmin@pmpractice.ru>
2019-04-25 18:18:25 +0300

    ajax

--- JS/promisefication.js
+++ JS/promisefication.js
@@ -1,40 +1,41 @@
-
 /*Промисификация – это когда берут асинхронный функционал и делают для него обёртку, возвращающую промис.
 После промисификации использование функционала зачастую становится гораздо удобнее.
 В качестве примера сделаем такую обёртку для запросов при помощи XMLHttpRequest.
 Функция httpGet(url) будет возвращать промис, который при успешной загрузке данных с url будет переходить в fulfilled с этими данными,
 а при ошибке – в rejected с информацией об ошибке: */
-debugger;   
+debugger;
+
 function httpGet(url) {
 
-    return new Promise(function(resolve, reject) {
-  
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', url, true);
-  
-      xhr.onload = function() {
-        if (this.status == 200) {
-          resolve(this.response);
-        } else {
-          var error = new Error(this.statusText);
-          error.code = this.status;
-          reject(error);
-        }
-      };
-  
-      xhr.onerror = function() {
-        reject(new Error("Network Error"));
-      };
-  
-      xhr.send();
+    return new Promise(function (resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+        
+        xhr.onload = function () {
+            if (this.status == 200) {
+                resolve(this.response);
+            } else {
+                var error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
+
+        xhr.onerror = function () {
+            reject(new Error("Network Error"));
+        };
+
+        xhr.send();
     });
-  
+
 }
-  /*Как видно, внутри функции объект XMLHttpRequest создаётся и отсылается как обычно,
-    при onload/onerror вызываются, соответственно, resolve (при статусе 200) или reject. */
+/*Как видно, внутри функции объект XMLHttpRequest создаётся и отсылается как обычно,
+  при onload/onerror вызываются, соответственно, resolve (при статусе 200) или reject. */
 
 httpGet("server/user.json")
-.then(
-    response => alert(`Fulfilled: ${response}`),
-    error => alert(`Rejected: ${error}`)
-);
+    .then(
+        response => alert(`Fulfilled: ${response}`),
+        error => alert(`Rejected: ${error}`)
+    );
